b450acb303072805f60f622a053d76f2303f11f9
evaristodisciullo-ctrl <evaristodisciullo@gmail.com>
2026-09-26 05:58:35 +0200

    Aggiorna cache PWA e supporto offline completo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='in-ordine-v1';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='in-ordine-v2';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./native-bridge.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
